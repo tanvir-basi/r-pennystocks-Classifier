36aea28839fe87ceb56a3d2256338807a52b063f
Tanvir Basi <95594161+tanvir-basi@users.noreply.github.com>
2022-09-10 20:11:10 +0100

    Update config.js

--- penny stocks website/config.js
+++ penny stocks website/config.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const config = {
     db: {
       /* don't expose password or any sensitive info, done only for demo */
-      host: "tanvir.mysql.database.azure.com",
-      user: "tb1264",
-      password: "Kutta9ikan",
-      database: "pennystocks",
+      host: "",
+      user: "",
+      password: "",
+      database: "",
       ssl: {ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
     },
     listPerPage: 10,
